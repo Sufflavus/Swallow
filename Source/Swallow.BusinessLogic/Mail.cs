fea6143d0853726ff12d14ffe1a8d01a876c3423
Sufflavus <tatashat@gmail.com>
2015-09-03 22:44:02 +0300

    + QueueManager

--- Source/Swallow.BusinessLogic/Mail.cs
+++ Source/Swallow.BusinessLogic/Mail.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace Swallow.BusinessLogic
 {
@@ -8,5 +10,19 @@ namespace Swallow.BusinessLogic
         public List<string> Receivers { get; set; }
         public string Sender { get; set; }
         public string Subject { get; set; }
+
+        public byte[] ToBytes()
+        {
+            string jsonMail = JsonConvert.SerializeObject(this);
+            byte[] result = Encoding.UTF8.GetBytes(jsonMail);
+            return result;
+        }
+
+        public static Mail FromBytes(byte[] data)
+        {
+            string message = Encoding.UTF8.GetString(data);
+            var mail = JsonConvert.DeserializeObject<Mail>(message);
+            return mail;
+        }
     }
 }
---
+++ Source/Swallow.BusinessLogic/QueueManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Swallow.BusinessLogic
+{
+    public sealed class QueueManager
+    {
+        public void Enqueue(Mail mail)
+        {
+            var factory = new ConnectionFactory {HostName = QueueSettings.HostName};
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: QueueSettings.QueueName,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string jsonMail = JsonConvert.SerializeObject(mail);
+                    byte[] body = Encoding.UTF8.GetBytes(jsonMail);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: QueueSettings.RoutingKey,
+                                         basicProperties: null,
+                                         body: body);
+                }
+            }
+        }
+
+        public void InitializeQueueListener()
+        {
+            var factory = new ConnectionFactory {HostName = QueueSettings.HostName};
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: QueueSettings.QueueName,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += OnReceived;
+
+                    channel.BasicConsume(queue: QueueSettings.QueueName,
+                                         noAck: true,
+                                         consumer: consumer);
+                }
+            }
+        }
+
+        private static void OnReceived(object model, BasicDeliverEventArgs ea)
+        {
+            byte[] body = ea.Body;
+            string message = Encoding.UTF8.GetString(body);
+            var mail = JsonConvert.DeserializeObject<Mail>(message);
+            MailProcessor.Process(mail);
+            //TODO: undelivered messages
+        }
+    }
+}
