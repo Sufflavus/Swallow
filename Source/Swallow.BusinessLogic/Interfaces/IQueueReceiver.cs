92c26c9ae1787b4cec65d54f364a13b28733ae78
Sufflavus <tatashat@gmail.com>
2015-09-22 23:09:01 +0300

    * Queue receiver has been changed

---
+++ Source/Swallow.BusinessLogic/Interfaces/IQueueReceiver.cs
@@ -0,0 +1,7 @@
+﻿namespace Swallow.BusinessLogic.Interfaces
+{
+    public interface IQueueReceiver
+    {
+        void Dequeue();
+    }
+}
---
+++ Source/Swallow.BusinessLogic/QueueReceiver.cs
@@ -0,0 +1,26 @@
+﻿using Swallow.BusinessLogic.Interfaces;
+using Swallow.QueueManager.Interfaces;
+
+namespace Swallow.BusinessLogic
+{
+    public sealed class QueueReceiver : IQueueReceiver
+    {
+        private readonly IQueueFactory _factory;
+        private readonly IMailProcessor _mailProcessor;
+
+        public QueueReceiver(IQueueFactory queueFactory, IMailProcessor mailProcessor)
+        {
+            _factory = queueFactory;
+            _mailProcessor = mailProcessor;
+        }
+
+        public void Dequeue()
+        {
+            using (IQueueWrapper queue = _factory.CreateReceiver(QueueSettings.QueueName))
+            {
+                var mail = queue.Dequeue<Mail>();
+                _mailProcessor.Process(mail);
+            }
+        }
+    }
+}
