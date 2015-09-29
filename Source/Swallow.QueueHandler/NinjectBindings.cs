415015100360edca5404a57b452edb3da7f5cccc
Sufflavus <tatashat@gmail.com>
2015-09-29 22:41:49 +0300

    + Queue handler

---
+++ Source/Swallow.QueueHandler/NinjectBindings.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using Swallow.BusinessLogic;
+using Swallow.BusinessLogic.Interfaces;
+
+namespace Swallow.QueueHandler
+{
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IQueueReceiver>().To<QueueReceiver>();
+        }
+    }
+}
---
+++ Source/Swallow.QueueHandler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Ninject;
+using Swallow.BusinessLogic.Interfaces;
+
+namespace Swallow.QueueHandler
+{
+    internal class Program
+    {
+        private static IQueueReceiver _queueReceiver;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("QueueHandler is running");
+            Console.WriteLine("Press any key to exit\n");
+
+            InitQueueReceiver();
+
+            while (!(Console.KeyAvailable))
+            {
+                _queueReceiver.Dequeue();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void InitQueueReceiver()
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            _queueReceiver = kernel.Get<IQueueReceiver>();
+        }
+    }
+}
